3285309c66b69a83ce74f833d471444435c158ce
Chris Harrison <cah@stpaulsschool.org.uk>
2021-03-07 11:28:18 +0000

    Base/Derived solution example

---
+++ Polymorphism/VirtualNewOverride/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace VirtualNewOverride
+{
+    class Program
+    {
+        // based on the example here: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/knowing-when-to-use-override-and-new-keywords 
+        static void Main(string[] args)
+        {
+            #region 1
+            // Step 1 create the instances of the objects 
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+
+            // this is the one that's going to mess with your heads. 
+            BaseClass bcdc = new DerivedClass();
+
+
+            Console.WriteLine("bc 1/2");
+            bc.Method1();
+            bc.Method2();
+
+            Console.WriteLine("\ndc 1/2");
+            dc.Method1();
+            dc.Method2();
+
+            Console.WriteLine("\nbcdc 1/2");
+            bcdc.Method1();
+            #endregion
+
+            #region 3 predict the output 
+            //// #3            
+            bcdc.Method2();
+
+            #endregion
+
+            #region 7 lists
+
+            // step 7 - putting them in a list 
+
+            // List<BaseClass> lb = new List<BaseClass> { bc, dc, bcdc };
+            // List<DerivedClass> ld = new List<DerivedClass> {  bc, dc, bcdc};
+
+            #endregion
+
+
+            #region 8 
+            // step 8 method 3 - will work with ld, but not with lb
+            #endregion
+
+            #region 9 abstract
+            // step 9 note we've not even talked about abstract ... 
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// A base class which implements 2 functions 
+        /// </summary>
+        class BaseClass
+        {
+            #region 5 virtual 
+            // step 5 - what effect does the virtual keyword have? 
+            // the importance of new vs override in the derived class. 
+            public virtual void Method1() 
+            {
+                Console.WriteLine("Base - Method1");
+            }
+            #endregion
+
+            #region 2 method 2 - a straight method (not virtual - see later, #5) 
+            // step 2 - a straight function - base.method2 is always coming here 
+            public void Method2()
+            {
+                Console.WriteLine("Base - Method2");
+            }
+            #endregion
+
+        }
+
+        /// <summary>
+        /// a derived class, which inherits from the base class 
+        /// </summary>
+        class DerivedClass : BaseClass
+        {
+            #region 6 override 
+            // // #6
+            public override void Method1()
+            {
+                Console.WriteLine("Derived - Method1");
+            }
+            #endregion
+
+            #region 4 new 
+            // #4 new 
+            #endregion
+            public new void Method2() 
+            {                
+                Console.WriteLine("Derived - Method2");
+            }
+
+            public void Method3()
+            {
+                Console.WriteLine("Derived - Method3");
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # polymorphism
+
+Polymorphism (from the Greek meaning 'many bodies') is one of the central tenets of object oriented programming. 
+
+The ideas presented here are around inheritence, but that inherited (or derived) classes can be considered instances of base classes, but that methods can be overridden and 'show through' even when the instance of a derived class is being used as if it were an instance of a base class. Keywords to use and understand are `base`, `abstract`, `virtual`, `override`, `new`. 
+
+Re-read that previous paragraph. When it makes sense, you will be in a good place. 
+
+
+### Example
+
+The code in the solution in this repo is based on the example on the MSDN article [When to use new and override](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/knowing-when-to-use-override-and-new-keywords). 
+
+### References 
+
+See also  
+
+### Exercise 
+
+[Inheritence Tasks](https://docs.google.com/document/d/1NSl9_k_jqfxKTqISihom_pRQY4mguAAKygddBPvkpEU/edit?usp=sharing).
